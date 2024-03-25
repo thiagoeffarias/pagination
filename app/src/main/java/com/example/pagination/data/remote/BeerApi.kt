5b61a5dc1c6635cb399fbd96f3e91c673a5ba37a
thiagofarias <thiagoferreirafarias@gmail.com>
2024-03-25 17:43:53 +0000

    bootstrap project

---
+++ app/src/main/java/com/example/pagination/data/remote/BeerApi.kt
@@ -0,0 +1,17 @@
+package com.example.pagination.data.remote
+
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface BeerApi {
+
+    @GET("/beers")
+    suspend fun getBeers(
+        @Query("page") page: Int,
+        @Query("per_page") pageCount: Int,
+    ) : List<BeerDto>
+
+    companion object {
+        const val BASE_URL = "https://punkapi.com/v2"
+    }
+}
---
+++ app/src/main/java/com/example/pagination/data/remote/BeerDto.kt
@@ -0,0 +1,10 @@
+package com.example.pagination.data.remote
+
+class BeerDto(
+    val id: Int,
+    val name: String,
+    val tagline: String,
+    val description: String,
+    val firstBrewed: String,
+    val imageUrl: String?
+)
